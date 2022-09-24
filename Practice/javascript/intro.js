0b2ada1d8260ed03cb93d9b2ec4a3104356360d0
Jose Alejandro Cifuentes Aluma <ing.josealejandro@hotmail.com>
2022-09-24 11:49:42 -0500

    Adding about section

---
+++ Practice/javascript/intro.js
@@ -0,0 +1,12 @@
+let nombre = "Alejandro";
+let apellidoMaterno = "Aluma";
+let nombreCompleto = nombre + ' ' + apellidoMaterno;
+console.log(nombreCompleto);
+
+let carName = 'Volvo';
+let x = 50;
+let a = 5;
+let b = 10;
+let y = a + b;
+let z = x + y;
+console.log(z);
